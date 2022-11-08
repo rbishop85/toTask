1a23d9db61dfd18393069188b7356b6f4a98bcc5
Jamie Otto <jmotto31@gmail.com>
2022-11-07 19:17:22 -0800

    adding initial single task component file

---
+++ client/src/components/SingleTask/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import { QUERY_SINGLE_TASK } from "../../utils/queries";
+
+const SingleTask = () => {
+
+   
+  };
+    
+
+export default SingleTask;
