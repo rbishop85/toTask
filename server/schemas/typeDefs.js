164aa83f12706e7b743baab2eb77c64380d262dd
Ben Holgate <holgateb@hotmail.com>
2022-11-07 20:09:18 -0800

    updated the resolvers, typedefs.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type User {
@@ -23,6 +23,13 @@ const typeDefs = gql`
     tag: Tag
     toerId: User
     doerId: User
+    comments: [Comment]
+  }
+  type Comment {
+    _id: ID
+    commentText: String
+    commentAuthor: String
+    createdAt: String
   }
 
   type Tag {
@@ -42,24 +49,33 @@ const typeDefs = gql`
     tasks: [Task]
     task(_id: String!): Task
     tags: [Tag]
-      }
-
-  type Comment {
-    _id: ID
-    commentText: String
-    createdAt: String
   }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    addTask(name: String!, description: String!, value: Int!, dueDate: String, tag: ID): Task
-    editTask(_id: ID!, name: String, description: String, value: Int, dueDate: String, completedDate: String, tag: ID, doerId: ID): Task
+    addTask(
+      name: String!
+      description: String!
+      value: Int!
+      dueDate: String
+      tag: ID
+    ): Task
+    editTask(
+      _id: ID!
+      name: String
+      description: String
+      value: Int
+      dueDate: String
+      completedDate: String
+      tag: ID
+      doerId: ID
+    ): Task
     deleteTask(taskId: ID!): Task
     updateUserPhoto(photoUrl: String!): User
-    addThought(thoughtText: String!, thoughtAuthor: String!): Thought
     addComment(thoughtId: ID!, commentText: String!): Thought
-      }
+    removeComment(thoughtId: ID!, commentId: ID!): Thought
+  }
 `;
 
 module.exports = typeDefs;
