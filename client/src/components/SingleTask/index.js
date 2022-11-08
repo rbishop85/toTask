f754f3309a1980a0eeea9a2c2eee307285ce2d89
Jamie Otto <jmotto31@gmail.com>
2022-11-07 19:27:39 -0800

    single task component added

--- client/src/components/SingleTask/index.js
+++ client/src/components/SingleTask/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
@@ -6,9 +6,32 @@ import { useQuery } from '@apollo/client';
 import { QUERY_SINGLE_TASK } from "../../utils/queries";
 
 const SingleTask = () => {
+    const { taskId } = useParams();
 
-   
+    const { loading, data } = useQuery(QUERY_SINGLE_TASK, {
+
+      variables: { taskId: taskId },
+    });
+  
+    const task = data?.task || {};
+  
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div >
+        <h3 >
+          {task.name}
+          {task.description}
+          {task.value}
+          {task.postDate}
+          {task.dueDate}
+          {task.completeDate}
+          {task.tag}
+          {task.toerId}
+          {task.doerId}
+        </h3>
+      </div>
+    );
   };
-    
-
 export default SingleTask;
