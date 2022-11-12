273e478bd60b496aaeb052d4f7b80b537230996b
Ben Holgate <holgateb@hotmail.com>
2022-11-12 13:32:54 -0800

    fixed tasks page padding at bottom above footer.

--- client/src/pages/TaskPage.js
+++ client/src/pages/TaskPage.js
@@ -7,11 +7,11 @@ import TaskList from '../components/TaskList';
 function TaskPage() {
     return (
         <main>
-        <div className='task-form mb-3 pl-5 pr-5 pb-5 border rounded border-3'>
+        <div className='task-form pl-5 pr-5 pb-5 border rounded border-3'>
             <TaskForm />
             
         </div>
-        <div><TaskList /></div>
+        <div className='mb-4'><TaskList /></div>
         </main>
       );
 };
